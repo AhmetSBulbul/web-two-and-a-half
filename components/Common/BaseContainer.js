f1c0b2bf76bffb83ec00b6fbf71ef8f5c9b058d2
AhmetSBulbul <ahmetsafabulbul@gmail.com>
2022-09-01 07:02:45 +0300

    storybook init

---
+++ components/Common/BaseContainer.js
@@ -0,0 +1,6 @@
+const BaseContainer = () => {
+    return (
+        <div className=" w-52 h-52 bg-red-400">Test</div>
+    )
+}
+export default BaseContainer
---
+++ stories/Container.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import BaseContainer from '../components/Common/BaseContainer';
+
+export default {
+    title: 'Base Container',
+    component: BaseContainer
+}
+
+export const Base = () => <BaseContainer/>
