ea9186509089c4827c44c5103e9e423ce000dfca
AhmetSBulbul <ahmetsafabulbul@gmail.com>
2022-09-09 01:03:46 +0300

    local gifs

--- pages/gif-the-pin.js
+++ pages/gif-the-pin.js
@@ -3,15 +3,19 @@ import React, { useEffect, useState } from 'react';
 import { Button } from '../components/button/Button';
 
 const TEST_GIFS = [
+    "https://media.giphy.com/media/SDogLD4FOZMM8/giphy.gif",
     "https://media.giphy.com/media/gqaLc5iPzDUsZ7CD7a/giphy.gif",
     "https://media.giphy.com/media/l3q2uMTpuoNwv6uDm/giphy.gif",
     "https://media.giphy.com/media/lMsT2f47tDxFMYdJMC/giphy.gif",
+    "https://media.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif",
 ]
 
-export default function GifThePin (){
+export default function GifThePin() {
     const [phantomWalletAddress, setPhantomWalletAddress] = useState(null);
+    const [inputValue, setInputValue] = useState("");
+    const [gifList, setGifList] = useState([]);
     const checkIfPhantomIsConnected = async () => {
-      
+
         try {
             const { solana } = window;
             if (solana) {
@@ -37,7 +41,7 @@ export default function GifThePin (){
     const connectPhantomWallet = async () => {
         const { solana } = window;
 
-        if(solana) {
+        if (solana) {
             const response = await solana.connect();
             console.log('Connected with Public Key: ', response.publicKey.toString())
             setPhantomWalletAddress(response.publicKey.toString());
@@ -45,22 +49,56 @@ export default function GifThePin (){
         }
     }
 
+    const onInputChange = (e) => {
+        const {value} = event.target;
+        setInputValue(value);
+    }
+
+    const sendGif = async () => {
+        if (inputValue.length > 0) {
+            console.log('Gif link:', inputValue);
+            setGifList([...gifList, inputValue]);
+            setInputValue("");
+        } else {
+            alert('Please enter a gif link');
+        }
+    }
+
     const renderConnectedContainer = () => (
-        <div className="grid grid-cols-3 gap-24 container items-center">
-            {TEST_GIFS.map((gif, index) => (
-                <div className="border-4 border-black drop-frame" key={gif}>
-                    <img src={gif} alt="gif" className='block'/>
-                    {/* <Image src={gif} layout="fill" style={{height:"140px", objectFit:"contain", height:"auto"}} objectFit='contain' /> */}
-                </div>
-            ))}
+        <div className='container mx-auto flex flex-col'>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                sendGif();
+            }}>
+                <input type="text" value={inputValue} onChange={onInputChange} placeholder='Enter gif link' className='drop-frame px-4 py-2 framed bg-surface mx-4'/>
+                <Button type='submit' label='Submit'/>
+            </form>
+            <div className="grid grid-cols-3 gap-24 place-items-center object-contain">
+                {gifList.map((gif, index) => (
+                    <div className="border-4 border-black drop-frame" key={gif}>
+                        <img src={gif} alt="gif" className='object-contain' />
+                        {/* <Image src={gif} layout="fill" style={{height:"140px", objectFit:"contain", height:"auto"}} objectFit='contain' /> */}
+                    </div>
+                ))}
+            </div>
         </div>
     )
 
     const renderNotConnectedContainer = () => (
         <div className='flex flex-col justify-center items-center'>
-                    <Button onClick={connectPhantomWallet} label="Connect Phantom Wallet"/>
-                </div>
+            <Button onClick={connectPhantomWallet} label="Connect Phantom Wallet" />
+        </div>
     )
+
+   
+
+    useEffect(() => {
+        if(phantomWalletAddress) {
+            console.log('Fetching Gif List...');
+            setGifList(TEST_GIFS);
+            
+        }
+    }, [phantomWalletAddress]);
 
 
 
@@ -75,9 +113,9 @@ export default function GifThePin (){
         <div className='app'>
             <div className='app-container'>
                 <div className='app-header'>Header</div>
-                {phantomWalletAddress ? renderConnectedContainer() : renderNotConnectedContainer()}
+                <div className='my-12'>{phantomWalletAddress ? renderConnectedContainer() : renderNotConnectedContainer()}</div>
                 <div className='app-footer'>Footer</div>
             </div>
         </div>
-        );
+    );
 }
